f205b97829c86f6507616e25f4b2794d7d64c311
denis-fominykh <fominykhdenis@gmail.com>
2020-06-01 13:47:17 +0300

    creating a header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,68 @@
 import React, { FC } from 'react';
+
 import './App.scss';
+import logo from './img/molfarlogoclean.png';
 
 export const App: FC = () => {
   return (
-    <div className="App">
-      <h1>Hello, React!</h1>
-    </div>
+    <React.Fragment>
+      <header className="header header_color">
+        <nav className="header__navigation header__navigation_flex">
+          <div className="header__navigation-logo header__navigation-logo_position">
+            <a className="header__navigation-link" href="/#">
+              <img
+                className="header__navigation-logo-link header__navigation-logo-link_size"
+                src={logo}
+                alt="logo"
+              />
+            </a>
+          </div>
+          <div className="header__navigation-links header__navigation-links_position">
+            <a
+              className="header__navigation-link header__navigation-link_style"
+              href="/#"
+            >
+              My Memorials
+            </a>
+            <a
+              className="header__navigation-link header__navigation-link_style"
+              href="/#"
+            >
+              Memory App
+            </a>
+            <a
+              className="header__navigation-link header__navigation-link_style"
+              href="/#"
+            >
+              Services
+            </a>
+            <a
+              className="header__navigation-link header__navigation-link_style"
+              href="/#"
+            >
+              About Ecorial
+            </a>
+            <a
+              className="header__navigation-link header__navigation-link_style"
+              href="/#"
+            >
+              Account
+            </a>
+            <a
+              className="header__navigation-link header__navigation-link_style"
+              href="/#"
+            >
+              Help
+            </a>
+            <a
+              className="header__navigation-link header__navigation-add-link_style"
+              href="/#"
+            >
+              Add memorial
+            </a>
+          </div>
+        </nav>
+      </header>
+    </React.Fragment>
   );
 };
