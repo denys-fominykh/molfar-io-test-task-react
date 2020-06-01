ed93d94f4d0dcfcb49738f7312b5fad62c996114
denis-fominykh <fominykhdenis@gmail.com>
2020-06-01 13:27:43 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import './App.scss';
+
+export const App: FC = () => {
+  return (
+    <div className="App">
+      <h1>Hello, React!</h1>
+    </div>
+  );
+};
